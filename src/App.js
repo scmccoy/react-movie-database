4702939d8a5eb51b8d299e599c3b085b61bf459b
shane <shanemccoypdx@gmail.com>
2019-02-05 09:39:28 -0500

    Add movie component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,35 @@
 import React, { Component } from "react";
+import Movie from "./Movie";
 import logo from "./logo.svg";
 import "./App.css";
 
-// const welcome = "Welcome yo";
+const movies = [
+  {
+    id: 1,
+    title: "Star Wars"
+  },
+  {
+    id: 2,
+    title: "Star Wars: The Empire Strikes Back"
+  },
+  {
+    id: 3,
+    title: "Star Wars: Revenge of the Jedi"
+  }
+];
 
 class App extends Component {
-  state = {
-    input: "Hello"
-  };
-
-  submit = () => {
-    console.log("click", this.text.value);
-  };
-
-  updateInput = e => {
-    console.log("update input", e.target.value);
-    this.setState({
-      input: e.target.value
-    });
-  };
-
   render() {
-    const { input } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to Props" />
-          <input type="text" onChange={this.updateInput} value={input} />
-          <input type="text" ref={input => (this.text = input)} />
-          <button onClick={this.submit}>Show Value</button>
         </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return <h1>{text}</h1>;
   }
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Movie extends Component {
+  render() {
+    return <div>{this.props.movie.title}</div>;
+  }
+}
+export default Movie;
