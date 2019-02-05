eaa1f0413e22cfaf1982f9f78c61d4bf3f882e3d
shane <shanemccoypdx@gmail.com>
2019-02-05 08:59:35 -0500

    input with state

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import "./App.css";
 // const welcome = "Welcome yo";
 
 class App extends Component {
+  state = {
+    input: "Hello"
+  };
+
   submit = () => {
     console.log("click", this.text.value);
   };
 
+  updateInput = e => {
+    console.log("update input", e.target.value);
+    this.setState({
+      input: e.target.value
+    });
+  };
+
   render() {
+    const { input } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Welcome text="Welcome to Props" />
+          <input type="text" onChange={this.updateInput} value={input} />
           <input type="text" ref={input => (this.text = input)} />
           <button onClick={this.submit}>Show Value</button>
         </header>
