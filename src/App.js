ecf93cd8f2dad2de9d85835a29da6bd8a98f4f09
shane <shanemccoypdx@gmail.com>
2019-02-11 10:55:20 -0500

    Add redux react-redux. Create store

--- src/App.js
+++ src/App.js
@@ -6,25 +6,32 @@ import {
   Switch,
   Link,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import MoviesList from './components/MoviesList';
 import MovieDetails from './components/MovieDetails';
 import './App.css';
 
-const App = () => (
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/" className="App-main-title">
-          <h1>- React Movie Database -</h1>
-        </Link>
-      </header>
-      <Switch>
-        <Route exact path="/" component={MoviesList} />
-        <Route path="/:id" component={MovieDetails} />
+const hello = () => ('hello');
+const store = createStore(hello);
 
-      </Switch>
-    </div>
-  </Router>
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/" className="App-main-title">
+            <h1>- React Movie Database -</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route exact path="/" component={MoviesList} />
+          <Route path="/:id" component={MovieDetails} />
+
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
